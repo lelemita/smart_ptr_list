9c1780b6bf245552468df3e9b223fa65406fdb4a
lelemita <nywfox@gmail.com>
2021-12-31 15:12:14 +0900

    old double linked list

--- main.cpp
+++ main.cpp
@@ -5,18 +5,30 @@ using namespace std;
 int main(void)
 {
     cout << "Double Linked Sample code" << endl;
-    old::List<int> oldList;
+    old::List<int> oldListDesc;
+    old::List<int> oldListAsc;
     for (int i = 0; i < 10; i++)
     {
-        oldList.addFront(i);
+        oldListDesc.addFront(i);
+        oldListAsc.addLast(i);
     }
 
-    if(!oldList.chkEmpty()) {
-        oldList.moveFront();
-        do {
-            cout << oldList.getValue() << " ";
-        } while (oldList.moveNext());
+    if(oldListDesc.chkEmpty() || oldListAsc.chkEmpty()) {
+        cout << "input data fail" << endl;
+        return 0;
     }
+
+    oldListAsc.moveFront();
+    do {
+        cout << oldListAsc.getValue() << " ";
+    } while (oldListAsc.moveNext());
     cout << endl;
+
+    oldListDesc.moveLast();
+    do {
+        cout << oldListDesc.getValue() << " ";
+    } while (oldListDesc.movePrev());
+    cout << endl;
+    
     return 0;
 }
--- myList.h
+++ myList.h
@@ -9,9 +9,11 @@ namespace old
     {
         T info;
         Node *next;
+        Node *prev;
         Node(T val)
             : info(val)
             , next(NULL)
+            , prev(NULL)
         {}
     };
 
@@ -19,18 +21,23 @@ namespace old
     {
         private:
             Node<T> *head;
+            Node<T> *tail;
             Node<T> *pCurr;
         public:
             List()
                 : head(NULL)
+                , tail(NULL)
                 , pCurr(NULL)
             {};
             ~List();
 
             void addFront(const T &value);
+            void addLast(const T &value);
             void removeFront();
             void moveFront() { pCurr = head; }
+            void moveLast() { pCurr = tail; }
             bool moveNext();
+            bool movePrev();
             bool chkEmpty() const { return head == NULL; }
             bool chkIfFront() const { return pCurr == head; }
             bool chkIfLast() const { return pCurr == NULL || pCurr->next == NULL; }
@@ -51,10 +58,32 @@ namespace old
     template <class T> void List<T>::addFront(const T &value)
     {
         Node<T> *temp = new Node<T>(value);
+        if (head != NULL) {
+            head->prev = temp;
+        }
         temp->next = head;
         head = temp;
         if (pCurr == NULL) {
             pCurr = head;
+        }
+        if (tail == NULL) {
+            tail = head;
+        }
+    }
+
+    template <class T> void List<T>::addLast(const T &value)
+    {
+        Node<T> *temp = new Node<T>(value);
+        if (tail != NULL) {
+            tail->next = temp;
+        }
+        temp->prev = tail;
+        tail = temp;
+        if (pCurr == NULL) {
+            pCurr = tail;
+        }
+        if (head == NULL) {
+            head = tail;
         }
     }
 
@@ -72,6 +101,15 @@ namespace old
     {
         if (pCurr != NULL && pCurr->next != NULL) {
             pCurr = pCurr->next;
+            return true;
+        }
+        return false;
+    }
+
+   template<class T> bool List<T>::movePrev()
+    {
+        if (pCurr != NULL && pCurr->prev != NULL) {
+            pCurr = pCurr->prev;
             return true;
         }
         return false;
